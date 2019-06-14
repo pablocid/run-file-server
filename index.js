49753aedb0a22091f198c429c8f0086d34a23617
Pablo <pabloagronomo@gmail.com>
2019-06-14 15:17:19 -0400

    env vars

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const { join } = require('path');
 const express = require('express');
 const app = express();
 const port = 8001;
-const urlFragment = '/resultFiles/ameba/';
-const foldertoServePath = join(__dirname, 'ameba');
+
+// const urlFragment = '/resultFiles/ameba/';
+// const foldertoServePath = '/home/ameba/ameba-data/resultFiles/ameba/';
+const urlFragment = process.env.URLFRAGMENT;
+const foldertoServePath = process.env.FOLDERPATH;
+ 
 
 app.use(urlFragment, express.static(foldertoServePath));
 
