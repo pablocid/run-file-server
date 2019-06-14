3b2dd340b7d058447232cec752edd70a3426f1bf
Pablo <pabloagronomo@gmail.com>
2019-06-14 15:53:47 -0400

    ready for env.json

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const { join } = require('path');
 const express = require('express');
-const app = express();
-const port = 8001;
 
-// const urlFragment = '/resultFiles/ameba/';
-// const foldertoServePath = '/home/ameba/ameba-data/resultFiles/ameba/';
+const app = express();
+const port = process.env.PORT;
 const urlFragment = process.env.URLFRAGMENT;
 const foldertoServePath = process.env.FOLDERPATH;
  
-
 app.use(urlFragment, express.static(foldertoServePath));
 
 app.listen(port, function () {
